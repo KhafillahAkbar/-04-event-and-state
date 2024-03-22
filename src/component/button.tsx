052d563f922561b2ef90199b23edeab2422458be
KhafillahAkbar <fillaakbar095@gmail.com>
2024-03-22 11:01:51 +0700

    W04 : Praktikum 2

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,23 +1,16 @@
-export default function Tombol_1() {
+export function Tombol_2({isiPesan, namaTombol}){
+    return(
+        <button
+        className="className=bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
+        onClick={() => alert(isiPesan)}>
+        {namaTombol}
+        </button>
 
+    )
+}
+
+export default function Tombol_1() {
     function handleClick(){
         alert("Tombol telah ditekan ! ! !");
     }
-
-    function handleMouseOver(){
-        alert("Eits, mau mencet tombol ya ?");
-    }
-    return (
-        <button 
-        className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded"
-        onClick={handleClick}
-        //onMouseOver={handleMouseOver}
-        onMouseLeave={() =>{
-            alert("Loh, kok sudah pergi ! ! !")
-            }
-        }
-        >
-            Ini Tombol
-        </button>
-    )
 }
